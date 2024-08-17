02d0367ec7a51b40889123193e2578da26faa3bf
preetsinghmakkar <preetsinghmakkar154@gmail.com>
2024-08-17 23:32:06 +0530

    Counter Completed

--- tests/solana-counter.ts
+++ tests/solana-counter.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaCounter } from "../target/types/solana_counter";
+import { expect } from "chai";
 
-describe("solana-counter", () => {
+describe("solana_counter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.SolanaCounter as Program<SolanaCounter>;
+  const program = anchor.workspace.solana_counter as Program<SolanaCounter>;
+
+  const counter = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+      .initialize()
+      .accounts({ counter: counter.publicKey })
+      .signers([counter])
+      .rpc();
+
+    const account = await program.account.counter.fetch(counter.publicKey);
+    expect(account.count.toNumber()).to.equal(0);
+  });
+
+  it("Incremented the count", async () => {
+    const tx = await program.methods
+      .increment()
+      .accounts({ counter: counter.publicKey, user: provider.wallet.publicKey })
+      .rpc();
+
+    const account = await program.account.counter.fetch(counter.publicKey);
+    expect(account.count.toNumber()).to.equal(1);
   });
 });
